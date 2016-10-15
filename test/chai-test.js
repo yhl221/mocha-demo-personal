71ec20302c79d0484fc31f454c563db09adea482
yhl221 <1727538058@qq.com>
2016-10-15 09:55:36 +0800

    modify chai-test.js

--- test/chai-test.js
+++ test/chai-test.js
@@ -5,4 +5,8 @@ describe('test the sum of two number',function () {
     it('2+2 should be 4',function () {
         expect(sum(2,2)).to.be.equal(4);
     });
+
+    it('2+2 should not be 10',function () {
+        expect(sum(2+2)).to.be.not.equal(10);
+    })
 });
